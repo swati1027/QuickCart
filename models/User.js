e8bb63a1f61af957d71b9099a715f0f3b667a9e3
Swati Matre <matreswati423@gmail.com>
2026-01-05 17:36:55 +0530

    First commit

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+const userSchemema=new mongoose.Schema({
+    _id:{type:String,required:true},
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    imageUrl:{type:String,required:true},
+    cartItems:{type:Object,default:{}},
+
+},{minimize:false})
+
+const User= mongoose.models.user||mongoose.model('user',userSchemema)
+
+export default User
