9ea39158fdfb814a03208864bf9f1e478344ffde
Swati Matre <matreswati423@gmail.com>
2026-05-22 19:43:55 +0530

    last

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,7 +1,7 @@
-import connectDB from "config/db";
-import Order from "models/Order";
-import Product from "models/Product";
-import { getAuth } from "clerk/nextjs/server";
+import connectDB from "@/config/db";
+import Order from "@/models/Order";
+import Product from "@/models/Product";
+import { getAuth } from "@/clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
