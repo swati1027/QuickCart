84c4b2f0e9b6aa5c283093e92b536f7f993f2795
Swati Matre <matreswati423@gmail.com>
2026-05-22 19:53:22 +0530

    first one

--- app/api/product/add/route.js
+++ app/api/product/add/route.js
@@ -1,6 +1,6 @@
 import connectDB from '@/config/db';
 import authSeller from '@/lib/authSeller';
-import { getAuth } from 'clerk/nextjs/server';
+import { getAuth } from '@clerk/nextjs/server';
 import { v2 as cloudinary } from 'cloudinary';
 import { NextResponse } from "next/server";
 import Product from '@/models/Product';
--- app/api/product/seller-list/route.js
+++ app/api/product/seller-list/route.js
@@ -1,7 +1,7 @@
 import connectDB from "@/config/db";
 import authSeller from "@/lib/authSeller";
 import { getAuth } from "@clerk/nextjs/server";
-import { NextResponse } from "@next/server";
+import { NextResponse } from "next/server";
 import Product from "@/models/Product";
 
 export async function GET(request) {
