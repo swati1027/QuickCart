5dbbb9db245fcaa22af9d6da4b9aabec546151fc
Swati Matre <matreswati423@gmail.com>
2026-05-25 11:11:58 +0530

    fix: change phone to phoneNumber in Address schema

--- models/Address.js
+++ models/Address.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const addressSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   fullName: { type: String, required: true },
-  phone: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
   pincode: { type: String, required: true },
   area: { type: String, required: true },
   city: { type: String, required: true },
